eb18a9ba37d918a80b472e6e0fd983690a8970a9
Asmae Mouncif <asmounci@e2r2p5.1337.ma>
2025-10-19 15:18:25 +0100

    second version

--- ft_split.c
+++ ft_split.c
@@ -6,36 +6,104 @@
 /*   By: asmounci <asmounci@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/17 18:18:09 by asmounci          #+#    #+#             */
-/*   Updated: 2025/10/17 19:50:23 by asmounci         ###   ########.fr       */
+/*   Updated: 2025/10/19 14:51:25 by asmounci         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <libft.h>
+#include "libft.h"
 
-char **ft_split(char const *s, char c)
+int count_words(char const *s, char c)
 {
     int i = 0;
-    while (s[i] != '\0' && s[i] == c)
+    int count = 0;
+    
+    while (s[i] != '\0')
     {
+        if (s[i] != c && (s[i + 1] == c || s[i + 1] == '\0'))
+            count++;
         i++;
     }
-    int l = ft_strlen(s);
-    char **p = malloc(2 * sizeof(char *));
-    
-    if(p == 0)
-        return NULL;
+    return count;
+}
 
-    while(s[i] != '\0' && s[i] != c)
+int word_length(char const *s, char c)
+{
+    int l = 0;
+    while (s[l] != '\0' && s[l] != c)
+    {
+        l++;
+    }
+    return l;
+}
+
+char *copy_word(char const *s, int len)
+{
+    char *p = malloc(len + 1);
+    if(p == NULL)
+        return NULL;
+    int i = 0;
+    while (i < len)
     {
         p[i] = s[i];
         i++;
     }
+    p[i] = '\0';
+    return p;
+}
+
+void    fail(char **p, int i)
+{
+    i--;
+    while(i >= 0)
+    {
+        free(p[i]);
+        i--;
+    }
+    free(p);
+}
+
+char **ft_split(char const *s, char c)
+{
+    int     i = 0;
+    int     j = 0;
+    char    **p;
+
+    if(s == NULL)
+        return NULL;
+    p = malloc(sizeof(char *) * (count_words(s, c) + 1));
+    if(p == 0)
+        return NULL;
+    while (s[j] != '\0')
+    {
+        int len = word_length(&s[j], c);
+        if (len > 0)
+        {
+            p[i] = copy_word(&s[j], len);
+            if (p[i] = NULL)
+            {
+                fail(p, i);
+                return NULL;
+            }
+            j = j + len;
+            i++;
+        }
+        else
+            j++;   
+    }
+    p[i] = NULL;
+    return p;
 }
 
 int main()
 {
-    char s[] = "++Salut--Bonjour+-42";
-    char c = '+';
-    printf("%s", ft_split(s, c));
-    return 0;
+    char s[] = "   zxcv      ar     t      t     b";
+    char c = ' ';
+
+    char **t = ft_split(s, c);
+    int i = 0;
+    while(t[i])
+    {
+        printf("%s\n", t[i]);
+        i++;
+    }
 }
